92b0dfaea1f64cb70b9ae37af69ca62bbb759c2d
asamarsal <ASAMARSAL@gmail.com>
2025-02-22 22:39:14 +0700

    Experiment

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
+const hbs = require('hbs');
+const path = require('path');
 
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './views'));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('assets'));
+app.use(express.static('public'));
 
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+
+//-----------------------------------------------------//
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.render('index');
 });
 
 app.get('/blog', (req, res) => {
-  res.sendFile(path.join(__dirname, 'blog.html'));
+  res.render('blog');
 });
 
 app.get('/testimonials', (req, res) => {
-  res.sendFile(path.join(__dirname, 'testimonials.html'));
+  res.render('testimonials');
 });
 
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+//-----------------------------------------------------//
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });

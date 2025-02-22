69bc60d747c7f6592ae7c5c47e7c0df547bc3f71
asamarsal <ASAMARSAL@gmail.com>
2025-02-22 21:32:45 +0700

    Experiment

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
-app.use(express.static('assets'));
-app.use(express.static('public'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/blog', (req, res) => {
-  res.render('blog');
+  res.sendFile(path.join(__dirname, 'blog.html'));
 });
 
 app.get('/testimonials', (req, res) => {
-  res.render('testimonials');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
+  res.sendFile(path.join(__dirname, 'testimonials.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });

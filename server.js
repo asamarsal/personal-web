acb1243058b6756ab4c1f67759a5354161fa78d1
asamarsal <ASAMARSAL@gmail.com>
2025-02-23 11:53:42 +0700

    Testing Vercel

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const hbs = require('hbs');
 const path = require('path');
 const port = 3000;
 
+const {formatDateToWIB, getRelativeTime} = require('./utils/time');
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('assets'));
 
 hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+
+let blogs = [];
+
+hbs.registerHelper("equal", function (a, b) {
+  return a === b;
+});
+
+hbs.registerHelper("formatDateToWIB", formatDateToWIB)
+hbs.registerHelper("getRelativeTime", getRelativeTime)
 
 //-----------------------------------------------------//
 app.get('/', (req, res) => {
@@ -17,7 +31,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/blog', (req, res) => {
-  res.render('blog');
+  console.log(blogs);
+  res.render('blog-list', {blogs: blogs});
+
+});
+
+app.get('/blog-create', (req, res) => {
+  res.render('blog-create');
+});
+
+app.post('/blog-create', (req, res) => {
+  const { title, content } = req.body;
+
+  let image = "https://picsum.photos/200/300";
+
+  let newBlog = {
+    title: title,
+    content: content,
+    image: "https://picsum.photos/200/300",
+    author: "Asa Marsal",
+    postedAt: new Date(),
+  };
+
+  blogs.push(newBlog);
+
+  res.redirect('/blog');
 });
 
 app.get('/testimonials', (req, res) => {
@@ -26,6 +64,10 @@ app.get('/testimonials', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.get('/blog-detail', (req, res) => {
+  res.render('blog-detail');
 });
 
 //-----------------------------------------------------//
